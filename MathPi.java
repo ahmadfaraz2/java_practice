56d64aaec705a9e4a1e3fb04b85278913094c007
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2023-05-28 15:41:49 +0500

    Using static with import

--- MathPi.java
+++ MathPi.java
@@ -1,11 +1,19 @@
-import java.lang.Math;
+import static java.lang.Math.PI;
+import static java.lang.Math.sqrt;
 
+/*
+ * By putting together [import] and [static] we can declare a fully
+ *  qualified name of a class and the method or the variable we
+ *  are interested in using in the code. This allows us to use the variable
+ *  or method directly, without the name of the class it is being
+ *  declared in.
+ */
 
 public class MathPi{
     public static void main(String[] args){
-        System.out.println("PI value = " + Math.PI);
+        System.out.println("PI value = " + PI);
 
-        double result = Math.sqrt(5.0);
+        double result = sqrt(5.0);
         System.out.println("SQRT value = " + result);
     }
 }
